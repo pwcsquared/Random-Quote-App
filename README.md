2779cb3765445e28c8f3acbd67d6ee5d5fec6d21
pwcsquared <pwcsquared@gmail.com>
2016-05-04 16:32:18 -0500

    rewrote js and html to use angular.js instead of jQuery

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Random-Quote-App
-An app that displays a random famous quote with the option to tweet the quote or get a new one
+An app that displays a random famous quote with the option to tweet the quote or get a new one.
+
+Recently re-written to use angular.js instead of jQuery.
--- random-quote.js
+++ random-quote.js
@@ -1,23 +1,25 @@
-$(document).ready(function(){
-  var getQuote = function(){
-    $.ajax({
-      type: "GET",
-      headers:{
-        "X-Mashape-Key": 'IJOV3xrIjCmshFcgsLQaeiB14q7Ap12dLt1jsn7CscrC4AZ2Vx',
-      },
-      dataType: 'json',
-     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous',
-      success: function(data){
-        $("#quote").html('"' + data.quote + '"');
-        $("#author").html("-" + data.author);
-        var tweetStub = "https://twitter.com/intent/tweet?text=";
-        var tweetQuote = tweetStub + '"' + data.quote + '"' + " -" + data.author;
-        $(".tweetQuote").on("mouseover",function(){
-          $(".tweetQuote").attr("href", tweetQuote);
-        });
-      }    
-    });
+var app = angular.module("quoteApp",[]);
+
+app.factory("getQuote",function($http){
+  var obj = {};
+  obj.myFunc = function(){
+    var config = {
+      headers: {"X-Mashape-Key": 'IJOV3xrIjCmshFcgsLQaeiB14q7Ap12dLt1jsn7CscrC4AZ2Vx'}
+    };
+    var url = "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous";
+    return $http.get(url, config);
   };
-  getQuote();
-  $(".getQuote").on("click",getQuote);
+  return obj;
+});
+
+app.controller("myCtrl",function($scope,$http, getQuote){
+  $scope.quote = "";
+  $scope.author = "";
+  $scope.tweet ='https://twitter.com/intent/tweet';
+  $scope.newQuote = function(){
+    getQuote.myFunc().success(function(response){
+      $scope.quote = response.quote;
+      $scope.author = response.author;
+    });
+  }
 });
